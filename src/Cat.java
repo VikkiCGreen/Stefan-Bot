76ed684e46895ccbf288152e48f4e8d410f817da
nikioni <vikkicgreen@gmail.com>
2017-03-29 21:43:42 -0400

    commit

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+
+public class Cat extends Animal {
+	
+	public Cat(String name) {
+		
+		this.name = name;
+	}
+
+}
---
+++ src/Shelter.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Shelter {
+	
+	HashMap<String, Animal> shelter = new HashMap<String, Animal>();
+
+	//main
+	public static void main(String[] args) {
+		
+		Cat cat1 = new Cat("Pookie");
+		Cat cat2 = new Cat("Mikasa");
+		Cat cat3 = new Cat("Nala");
+		Dog dog1 = new Dog("Shelby");
+		Dog dog2 = new Dog("Mynx");
+		Dog dog3 = new Dog("Atreyu")
+		
+	}
+
+}
